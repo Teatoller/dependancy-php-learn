ca1c50cbac4374881a187b3109fcfc274f0eb768
Teatoller <steven.otieno1@gmail.com>
2019-08-27 14:41:08 +0300

    (import dependancy): implement file imports in index file
    - add require src files in index
    
    [Starts #005]

--- index.php
+++ index.php
@@ -1,54 +1,8 @@
 <?php
 
-class Person
-{
-    protected $name;
-
-    public function __construct($name)
-    {
-        $this->name = $name;
-    }
-}
-
-class Business
-{
-    protected $staff;
-
-    public function __construct(Staff $staff)
-    {
-        $this->staff = $staff;
-    }
-
-    public function hire(Person $person)
-    {
-        $this->staff->add($person);
-    }
-
-    public function getStaffMembers()
-    {
-        return $this->staff->members();
-    }
-}
-
-class Staff
-{
-    protected $members = [];
-
-    public function __construct($members = [])
-    {
-        $this->member = $members;
-    }
-
-    public function add(Person $person)
-    {
-        $this->member[] = $person;
-    }
-
-    public function members()
-    {
-        return $this->members;
-    }
-}
+require 'src/Person.php';
+require 'src/Staff.php';
+require 'src/Business.php';
 
 $steven = new Person('Steven Ennis');
 $staff = new Staff([$steven]);
